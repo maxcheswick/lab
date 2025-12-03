0f74c6af559f2be0e65df95a9da304013bf4c76e
Max Cheswick <maxcheswick@gmail.com>
2025-12-02 19:26:52 -0500

    basic stuff to refresh memory

---
+++ python/basic.py
@@ -0,0 +1,27 @@
+# File handleing
+with open("file.txt") as f:
+    for line in f:
+        print(line)
+        
+# Dictionaries
+d = {"a":1}
+d["b"] = 2
+for k, v in d.items():
+    print(f"Key={k}, Value={v}")
+
+#List Comprehension
+values = []
+filtered = [x for x in values if "error" in x]
+
+# Sort function = creates new list
+sorted_values = sorted(values)
+# Sort method = modifies current list
+values.sort()
+
+# Dedupe
+unique = set(values)
+
+#datetime
+from datetime import datetime
+dt = datetime.strptime("2025-09-17 04:20:00", "%Y-%m-%d %H:%M:%S")
+
